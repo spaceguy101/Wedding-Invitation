6eb60ca01ce61805915059da2373a4d8ea6d0077
shreyas <shreyas>
2022-11-20 02:04:39 +0530

    update pdfs

--- js/script.js
+++ js/script.js
@@ -36,7 +36,7 @@ soundButton.on('click', function (e) {
 
 
 // Set the date we're counting down to
-var countDownDate = new Date("Dec 4, 2022 00:00:00").getTime();
+var countDownDate = new Date("Dec 4, 2022 12:25:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function () {
